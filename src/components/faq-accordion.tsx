727cc850e030b68d7c969dea86c319383e8d4a06
John Angelo David <johnangelo.programming@gmail.com>
2025-10-19 23:44:27 +0800

    Mama Mica Glow

--- src/components/faq-accordion.tsx
+++ src/components/faq-accordion.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+/* eslint-disable react/no-unescaped-entities */
 import { useState } from 'react'
 import { Card, CardContent } from '@/components/ui/card'
 import { Badge } from '@/components/ui/badge'
